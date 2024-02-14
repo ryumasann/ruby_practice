60ace01be07290c45d7a6d1e5637ed136322adae
ryumasann <im20140319@gmail.com>
2024-02-14 20:40:06 +0900

    sheep add comment

--- ruby_study/sheep.rb
+++ ruby_study/sheep.rb
@@ -2,6 +2,8 @@ def sheep(n)
     x = 0
     (1..n).each{puts "羊が#{x += 1}匹"}
     # (1..n).each{puts "羊が#{x = x.next}匹"}
+    # nextは遅いらしい
+    # https://qiita.com/luccafort/items/11e3569095e5b5b67f8e
 end
 sheep(3)
 sheep(1)
