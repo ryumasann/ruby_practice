63c9f7024afe112ee66ef2fad6cb2abab1728b17
ryumasann <im20140319@gmail.com>
2024-02-14 22:27:35 +0900

    modify to  map

--- ruby_study/square.rb
+++ ruby_study/square.rb
@@ -1,7 +1,8 @@
 def square(numbers)
-    new_members = []
-    numbers.each{ |n| new_members << n ** 2}
-    new_members
+    # new_members = []
+    # numbers.each{ |n| new_members << n ** 2}
+    # new_members
+    new_members = numbers.map{ |n| n ** 2}
 end
 
 squared_numbers = square([5, 7, 10])
