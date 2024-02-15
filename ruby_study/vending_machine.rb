56aa42d8b5e659025254484b3d2ce2e7b45b38c7
ryumasann <im20140319@gmail.com>
2024-02-15 20:03:25 +0900

    initialize

--- ruby_study/vending_machine.rb
+++ ruby_study/vending_machine.rb
@@ -1,8 +1,23 @@
 class VendingMachine
+    def initialize(company_name)
+        @company_name = company_name
+    end
+
     def press_button()
         puts "cider"
     end
+
+    def press_manufacturer_name()
+        puts @company_name
+    end
   end
 
-vending_machine = VendingMachine.new
+vending_machine = VendingMachine.new('サントリー')
 puts vending_machine.press_button
+
+vending_machine.deposit_coin(150)
+puts vending_machine.press_button
+vending_machine.deposit_coin(100)
+puts vending_machine.press_button
+
+puts vending_machine.press_manufacturer_name
