d869eb8fbb6b777675326a1da221b83dbea175ed
ryumasann <im20140319@gmail.com>
2024-02-15 23:42:32 +0900

    inheritance

--- ruby_study/vending_machine_inheritance.rb
+++ ruby_study/vending_machine_inheritance.rb
@@ -1,14 +1,27 @@
 class VendingMachine
+    MAX_CUPS = 100
     def initialize(vender_name)
         @vender_name = vender_name
         @total_amount = 0
+        @cup_inventory = 0
     end
 
     def press_button(vending_item)
-            if @total_amount >= vending_item.price
-            puts vending_item.name
-            @total_amount -= vending_item.price
+        if @total_amount >= vending_item.price
+            if vending_item.is_a?(CupCoffee) && @cup_inventory > 0
+                puts "#{vending_item.name} cup coffee"
+                @cup_inventory -= 1
+                @total_amount -= vending_item.price
+            elsif vending_item.is_a?(Drink)
+                puts vending_item.name
+                @total_amount -= vending_item.price
+            end
         end
+    end
+
+    def add_cup(num)
+        @cup_inventory += num
+        puts @cup_inventory
     end
 
     def deposit_coin(total_amount)
@@ -33,6 +46,12 @@ class VendingItem
             150
         when 'cider'
             100
+        when 'cider'
+            100
+        when 'hot'
+            100
+        when 'cold'
+            100
         else
             puts name
             raise
@@ -42,25 +61,25 @@ end
 
 # 飲み物に関する責務のクラスを継承を利用して作成してください
 class Drink < VendingItem
-    attr_reader :status
-    def initialize(name, status)
-        super(name)
-        @status = status
+    attr_accessor :name, :price
+    def initialize(name)
+        super
+        @price = 100
     end
 end
 
 # カップコーヒーに関する責務のクラスを継承を利用して作成してください
 class CupCoffee < VendingItem
-    attr_accessor :cup_total_amount
-    def initialize(name, cup_total_amount)
-        super(name)
-        @cup_total_amount = cup_total_amount
+    attr_accessor :name, :price
+    def initialize(name)
+        super
+        @price = 100
     end
 end
 
 # input
-hot_cup_coffee = #{カップコーヒーのクラス}.new('hot');
-cider = #{飲み物のクラス}.new('cider')
+hot_cup_coffee = CupCoffee.new('hot');
+cider = Drink.new('cider')
 vending_machine = VendingMachine.new('サントリー')
 vending_machine.deposit_coin(100)
 vending_machine.deposit_coin(100)
