6207365a3b54f30b3e5a630f33d1c92d9da8ae3e
ryumasann <im20140319@gmail.com>
2024-02-17 23:27:01 +0900

    cubocot

--- ruby_study/vending_machine_polymorphism.rb
+++ ruby_study/vending_machine_polymorphism.rb
@@ -1,64 +1,62 @@
+# frozen_string_literal: true
+
 class VendingMachine
-    MAX_CUPS = 100
-    private_constant :MAX_CUPS
-    def initialize(vender_name)
-        @vender_name = vender_name
-        @total_amount = 0
-        @cup_inventory = 0
-    end
+  MAX_CUPS = 100
+  private_constant :MAX_CUPS
+  def initialize(vender_name)
+    @vender_name = vender_name
+    @total_amount = 0
+    @cup_inventory = 0
+  end
 
-    def press_button(vending_item)
-        if @total_amount >= vending_item.price
-            if vending_item.is_a?(CupCoffee) && @cup_inventory > 0
-                @cup_inventory -= 1
-                @total_amount -= vending_item.price
-                print "#{vending_item.name} cup coffee"
-            elsif vending_item.is_a?(Drink)
-                @total_amount -= vending_item.price
-                print vending_item.name
-            elsif vending_item.is_a?(Snack)
-                @total_amount -= vending_item.price
-                print vending_item.name
-            end
-        end
-    end
+  def press_button(vending_item)
+    return unless @total_amount >= vending_item.price
 
-    def add_cup(num)
-        @cup_inventory += num
+    if vending_item.is_a?(CupCoffee) && @cup_inventory.positive?
+      @cup_inventory -= 1
+      @total_amount -= vending_item.price
+      print "#{vending_item.name} cup coffee"
+    elsif vending_item.is_a?(Drink)
+      @total_amount -= vending_item.price
+      print vending_item.name
+    elsif vending_item.is_a?(Snack)
+      @total_amount -= vending_item.price
+      print vending_item.name
     end
+  end
 
-    def deposit_coin(total_amount)
-        if total_amount == 100
-            @total_amount += 100
-        end
-    end
+  def add_cup(num)
+    @cup_inventory += num
+  end
 
-    private
-    def press_manufacturer_name()
-        puts @vender_name
-    end
+  def deposit_coin(total_amount)
+    return unless total_amount == 100
+
+    @total_amount += 100
+  end
+
+  private
+
+  def press_manufacturer_name
+    puts @vender_name
+  end
 end
 
 class VendingItem
-    attr_reader :name, :price
-    def initialize(name)
-        @name = name
-        @price = case name
-        when 'cola'
-            150
-        when 'cider'
-            100
-        when 'cider'
-            100
-        when 'hot'
-            100
-        when 'cold'
-            100
-        else
-            puts name
-            raise
-        end
-    end
+  attr_reader :name, :price
+
+  def initialize(name)
+    @name = name
+    @price = case name
+             when 'cola'
+               150
+             when 'cider', 'hot', 'cold'
+               100
+             else
+               puts name
+               raise
+             end
+  end
 end
 
 # 飲み物に関する責務のクラスを継承を利用して作成してください
@@ -71,19 +69,20 @@ end
 
 # スナック菓子に関する責務のクラスを継承を利用して作成してください
 class Snack < VendingItem
-    PRICE = 150
-    private_constant :PRICE
-    NAME = "potato chips"
-    private_constant :NAME
-    attr_reader :NAME, :PRICE
-    def initialize()
-        @name = NAME
-        @price = PRICE
-    end
+  PRICE = 150
+  private_constant :PRICE
+  NAME = 'potato chips'
+  private_constant :NAME
+  attr_reader :NAME, :PRICE
+
+  def initialize
+    @name = NAME
+    @price = PRICE
+  end
 end
 
 # input
-hot_cup_coffee = CupCoffee.new('hot');
+hot_cup_coffee = CupCoffee.new('hot')
 cider = Drink.new('cider')
 snack = Snack.new
 vending_machine = VendingMachine.new('サントリー')
