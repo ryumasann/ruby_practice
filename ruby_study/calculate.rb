154aa5e039d280f056d860b1fdaffb8be25c1d7e
ryumasann <im20140319@gmail.com>
2024-02-18 13:23:47 +0900

    bundler

--- ruby_study/calculate.rb
+++ ruby_study/calculate.rb
@@ -1,31 +1,32 @@
-OPERATOR_LIST = ["+", "-", "*", "/"]
+OPERATOR_LIST = ['+', '-', '*', '/']
 def calculate(num1, num2, operator)
-    if (operator == "/") && (num2 == 0)
-        raise ZeroDivisionError, "ゼロによる割り算は許可されていません"
-    elsif !OPERATOR_LIST.include?(operator)
-        raise "演算子には  +、-、*、/ のいずれかを使用してください"
-    elsif !((num1 - num1.to_i == 0) && (num2 - num2.to_i == 0))
-        raise "num1、 num2 には整数を入力してください"
-    end
-    result = num1.send(operator, num2).to_i
+  if (operator == '/') && (num2 == 0)
+    raise ZeroDivisionError, 'ゼロによる割り算は許可されていません'
+  elsif !OPERATOR_LIST.include?(operator)
+    raise '演算子には  +、-、*、/ のいずれかを使用してください'
+  elsif !((num1 - num1.to_i == 0) && (num2 - num2.to_i == 0))
+    raise 'num1、 num2 には整数を入力してください'
+  end
+
+  result = num1.send(operator, num2).to_i
 end
 
-puts "1番目の整数を入力してください:"
+puts '1番目の整数を入力してください:'
 num1 = gets.chomp.to_f
 
-puts "2番目の整数を入力してください:"
+puts '2番目の整数を入力してください:'
 num2 = gets.chomp.to_f
 
-puts "演算子(+, -, *, /)を入力してください:"
+puts '演算子(+, -, *, /)を入力してください:'
 operator = gets.chomp
 
 begin
-    result = calculate(num1, num2, operator)
-    puts result
+  result = calculate(num1, num2, operator)
+  puts result
 rescue ZeroDivisionError => e
-    puts e.full_message
+  puts e.full_message
 rescue StandardError => e
-    puts e.full_message
+  puts e.full_message
 end
 
 # calculate(1, 0, '/')
