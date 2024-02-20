ae7c6b86f9f1ffe0d0693dc02bedf7d9909191a3
ryumasann <im20140319@gmail.com>
2024-02-20 23:56:45 +0900

    modigy player class

--- assignment/oop/war_game.rb
+++ assignment/oop/war_game.rb
@@ -13,33 +13,17 @@ class Player
     @cards_you_get = 0
   end
 
-  def draw(trump_class)
-    # trump_mark = trump_class.marks.sample
-    trump_mark = trump_class.marks[0]
-    pp trump_class.size
+  def draw(trump)
+    trump_mark = trump.marks.sample
     if trump_mark.cards.length.positive?
-      trump_class.size -= 1
-      pp trump_class.size
-      card = trump_mark.cards.pop
+      trump.size -= 1
+      value = trump_mark.cards.pop
+      { trump_mark_name: trump_mark.name, value: value }
     else
-      trump_class.marks = trump_class.marks.reject { |trump_mark| trump_mark.cards.empty? }
-      pp trump_class.marks
-      trump_class.marks
+      trump.marks = trump.marks.reject { |trump_mark| trump_mark.cards.empty? }
+      nil
     end
   end
-
-  # def draw(trump_marks, trump_size)
-  #   # trump_mark = trump_marks.sample
-  #   trump_mark = trump_marks[0]
-  #   pp trump_size
-  #   if trump_mark.cards.length.positive?
-  #     trump_size -= 1
-  #     pp trump_size
-  #     card = trump_mark.cards.pop
-  #   else
-  #     trump_marks = trump_marks.reject { |trump_mark| trump_mark.cards.empty? }
-  #   end
-  # end
 
   def get_cards(cards)
     @cards_you_get += cards
@@ -55,6 +39,7 @@ class Trump
   ASCENDING_CARDS_STRONGNESS = CARDS_IN_EACH_MARK.clone
 
   attr_accessor :cards, :size, :marks
+  attr_reader :name
 
   def initialize
     @cards = shuffle if self.class != Trump
@@ -74,27 +59,41 @@ class Trump
     trump_marks.each { |trump_mark| sum += trump_mark.cards.length }
     @size = sum
   end
-  # def define_size(trump_class)
-  #   sum = 0
-  #   trump_class.marks.each { |trump_mark| sum += trump_mark.cards.length }
-  #   @size = sum
-  # end
 
-  def which_strong?
-    ASCENDING_CARDS_STRONGNESS
+  def which_strong?(card1, card2)
+    puts "プレイヤー1のカードは#{card1[:trump_mark_name]}の#{card1[:value]}です。"
+    puts "プレイヤー2のカードは#{card2[:trump_mark_name]}の#{card2[:value]}です。"
+
+    # nil unless ASCENDING_CARDS_STRONGNESS.index(card1) > ASCENDING_CARDS_STRONGNESS.index(card2)
   end
 end
 
 class Spade < Trump
+  def initialize
+    super
+    @name = 'スペード'
+  end
 end
 
 class Heart < Trump
+  def initialize
+    super
+    @name = 'ハート'
+  end
 end
 
 class Diamond < Trump
+  def initialize
+    super
+    @name = 'ダイアモンド'
+  end
 end
 
 class Club < Trump
+  def initialize
+    super
+    @name = 'クラブ'
+  end
 end
 
 def war_start
@@ -117,50 +116,18 @@ def war_game
   pp trump.size
   card = ''
 
-  card = prayer1.draw(trump)
-  pp card
-  pp trump.size
-  card = prayer1.draw(trump)
-  pp card
-  pp trump.size
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  prayer1.draw(trump)
-  pp trump.marks
-  pp trump.size
+  loop do
+    prayer1_card = prayer1.draw(trump)
+    prayer2_card = prayer2.draw(trump)
 
-  # while trump.trump_size.positive?
-  #   # card_this_turn = [spade, heart, diamond, club].sample
-  #   prayer1.draw(trump_marks, trump.trump_size)
-  #   prayer2.draw(trump_marks, trump.trump_size)
-  #   # if card_this_turn.instance_of(Spade)
-  #   # elsif card_this_turn.instance_of(Heart)
-  #   #   prayer1.draw(Heart)
-  #   #   prayer2.draw(Heart)
-  #   # elsif card_this_turn.instance_of(Diamond)
-  #   #   prayer1.draw(Diamond)
-  #   #   prayer2.draw(Diamond)
-  #   # elsif card_this_turn.instance_of(Club)
-  #   #   prayer1.draw(Club)
-  #   #   prayer2.draw(Club)
-  #   # else
-  #   #   raise
-  #   # end
-  # end
-  # # TODO
-  # # 最後にお互いの持ち札の比較
-  # # プレイヤー1が勝ちました。
-  # puts '戦争を終了します。'
+    if prayer1_card && prayer2_card
+      trump.which_strong?(prayer1_card, prayer2_card)
+    else
+      puts 'プレイヤー1が勝ちました。'
+      puts '戦争を終了します。'
+      break
+    end
+  end
 end
 
 war_game
